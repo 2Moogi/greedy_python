117a68d1806f680395f325cc3e4ca29a37855133
2Moogi <78463220+2Moogi@users.noreply.github.com>
2023-04-13 13:45:47 +0900

    [Silver III] Title: 등수 매기기, Time: 568 ms, Memory: 73244 KB -BaekjoonHub

--- 백준/Silver/2012. 등수 매기기/등수 매기기.py
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.py
@@ -1,16 +1,16 @@
 import sys
-input=sys.stdin.readline
+input= sys.stdin.readline
 
 n=int(input())
-rank=[]
-dataset=[]
-real_rank=[]
+data=[]
+list=[]
 for i in range(n):
-    fore_rank=int(input())
-    rank.append(fore_rank)
-    dataset.append(i+1)
-rank.sort()
-for j in range(n):
-    onlyfire = abs(dataset[j]-rank[j])
-    real_rank.append(onlyfire)
-print(sum(real_rank))
+    k=int(input())
+    data.append(k)
+data.sort()
+for i in range(1,n+1):
+    list.append(i)
+result=0
+for i in range(n):
+    result = result + abs(list[i]-data[i])
+print(result)
